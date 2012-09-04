8e723ddcc1937bc51a1fa11abd459d54fbe9ac1c
Pawel Klosiewicz <pawelkl@gmail.com>
2012-09-04 14:26:12 +0200

    rails_apps_composer: initial commit

---
+++ config/environment.rb
@@ -0,0 +1,5 @@
+# Load the rails application
+require File.expand_path('../application', __FILE__)
+
+# Initialize the rails application
+Klosiewicz::Application.initialize!
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Klosiewicz::Application.config.secret_token = '63692ee635405800d83f05d059402a9228bc8b3f6887f60df46eb56e44ea4e3a2c872bd6e1c93a0928d018f5786c40cae37ee86a037d8e27b277b1ae9584c783'
